4f566a2dd886076ecfa973fffd78dbd3a57385eb
Sumit Prasad <sumitprasadd.wb24@gmail.com>
2025-10-04 19:06:26 +0530

    preprocess data

--- config/paths_config.py
+++ config/paths_config.py
@@ -5,7 +5,7 @@ import os
 
 RAW_DIR = 'artifacts/raw'
 RAW_FILE_PATH = os.path.join(RAW_DIR, "raw.csv")
-TRAIN_FILE_PATH = os.path.join(RAW_DIR, 'tarin.csv')
+TRAIN_FILE_PATH = os.path.join(RAW_DIR, 'train.csv')
 TEST_FILE_PATH = os.path.join(RAW_DIR, 'test.csv')
 
 # Config
--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -7,4 +7,136 @@ from src.custom_exception import CustomException
 from config.paths_config import *
 from utils.common_functions import read_yaml, load_data
 
-from sklearn.en
+from sklearn.ensemble import RandomForestRegressor
+from lightgbm import LGBMRegressor
+from sklearn.preprocessing import LabelEncoder
+
+
+logger = get_logger(__name__)
+
+
+class DataProcessor:
+
+    def __init__(self, train_path, test_path, processed_dir, config_path):
+        self.train_path = train_path
+        self.test_path = test_path
+        self.processed_dir = processed_dir
+
+        self.config = read_yaml(config_path)
+
+        if not os.path.exists(self.processed_dir):
+            os.makedirs(self.processed_dir)
+
+
+    def data_processing(self, df : pd.DataFrame):
+        try:
+            logger.info("Starting our data processing step")
+            logger.info("Dropping the columns")
+
+            df.drop(columns = ["Order_ID"], inplace = True)
+            df.drop_duplicates(inplace = True)
+
+            cat_cols = self.config['data_processing']['categorical_columns']
+            num_cols = self.config['data_processing']['numerical_columns']
+
+            logger.info("Applying LabelEncoding")
+
+            label_encoder = LabelEncoder()
+            mappings = {}
+
+            for col in cat_cols:
+                df[col] = label_encoder.fit_transform(df[col])
+                mappings[col] = {label : code for label, code in zip(label_encoder.classes_, label_encoder.transform(label_encoder.classes_))}
+
+            logger.info("Label Mappings are : ")
+
+            for col, mapping in mappings.items():
+                logger.info(f"{col} : {mapping}")
+
+            logger.info("Encoding done.")
+
+            return df
+
+        except Exception as e:
+            logger.error(f"Error During data preprocessing {e}")
+            raise CustomException("Error while preprocessing data.", e)
+        
+    
+
+    def select_features(self, df : pd.DataFrame):
+        try:
+            logger.info("Starting Our Feature Selection")
+
+            X = df.drop(columns = 'Delivery_Time')
+            y = df['Delivery_Time']
+
+            model = RandomForestRegressor(random_state = 42)
+            model.fit(X,y)
+
+            feature_importance = model.feature_importances_
+            features_importance_df = pd.DataFrame({
+                'features' : X.columns,
+                'importance' : feature_importance
+            })
+
+            top_important_features_df = features_importance_df.sort_values(by = 'importance', ascending = False)
+            num_features_to_select = self.config['data_processing']["no_of_features"]
+
+            top_15_features = top_important_features_df['features'].head(num_features_to_select).values
+
+            logger.info(f"Features Selected : {top_15_features}")
+
+            top_15_df = df[top_15_features.tolist() + ['Delivery_Time']]
+
+            logger.info("Feature Selection Completed Successfully.")
+
+            return top_15_df
+        
+        except Exception as e:
+            logger.error(f"Error During feature selection {e}")
+            raise CustomException("Error while feature selection.", e)
+        
+    
+    def save_data(self, df : pd.DataFrame, file_path):
+        try:
+            logger.info("Saving data into processed folder")
+
+            df.to_csv(file_path, index = False)
+
+            logger.info(f"Data Saved Successfully to {file_path}")
+
+        except Exception as e:
+            logger.error(f"Error During Saving Data {e}")
+            raise CustomException("Error while Saving Data.", e)
+        
+
+    
+    def process(self):
+        try:
+            logger.info("Loading the data from raw dir")
+
+            train_df = load_data(self.train_path)
+            test_df = load_data(self.test_path)
+
+            train_df = self.data_processing(train_df)
+            test_df = self.data_processing(test_df)
+
+            train_df = self.select_features(train_df)
+
+            test_df = test_df[train_df.columns]
+
+            self.save_data(train_df, PROCESSED_TRAIN_DATA_PATH)
+            self.save_data(test_df, PROCESSED_TEST_DATA_PATH)
+
+            logger.info("Data Processing completed successfully.")
+
+        
+        except Exception as e:
+            logger.error(f"Error During processing data pipeline {e}")
+            raise CustomException("Error while Processing Data Pipeline.", e)
+    
+
+if __name__ == "__main__":
+
+    processor = DataProcessor(TRAIN_FILE_PATH, TEST_FILE_PATH, PROCESSED_DIR, CONFIG_PATH)
+    processor.process()
